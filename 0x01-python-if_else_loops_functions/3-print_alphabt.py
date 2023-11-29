03ab323bb28ffb38c574bdade355e556eb6174e2
preshux <preshux@gmail.com>
2023-11-28 18:44:07 -0800

    updated task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 for alph in range (97, 123):
     if alph == 101 or alph == 113:
         alph += 1
         continue
     else:
         print("{:c}".format(alph), end="")
-    alph += 1
+        alph += 1
