01953589bf8c61afa2f778d8840c8d485b28f2dc
preshux <preshux@gmail.com>
2023-11-28 18:12:16 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-alph = 97
-
-while alph < 123:
+for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{:c}".format(letter), end="")
+        print("{}".format(chr(letter)), end="")i)
