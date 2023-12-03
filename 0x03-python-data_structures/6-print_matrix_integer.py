9782c61f86d11482a6dc43e7460e8053411cfbc6
preshux <preshux@gmail.com>
2023-12-03 09:48:59 -0800

    added 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
+        for number in row:
+            print("{:d}".format(number), end=" ")
         print()
